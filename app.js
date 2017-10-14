e5e7ac3ec14142586046ba66af51a2020552f2b9
Jason Leinberger <jason@leinbergerdev.com>
2017-10-13 22:26:27 -0500

    assignment-8-dom-scripting: complete

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function onReady() {
   const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
+  const rButton = document.getElementById('removeButton');
 
   function createNewToDo () {
       const newToDoText = document.getElementById('newToDoText');
@@ -17,6 +18,23 @@ function onReady() {
       renderTheUI(toDos);
   }
 
+  rButton.onclick = function removeCompletedTasks() {
+    var removeItemsArray = [];
+    for (var i = 0; i < toDos.length; i++) {
+      alert(toDos[i].complete)
+      if (toDos[i].complete == true) {
+        removeItemsArray.push(i);
+      }
+    }
+    if (removeItemsArray.length > 0){
+      for (var i = removeItemsArray.length - 1; i >= 0; i--) {
+        alert(removeItemsArray[i]);
+        toDos.splice(removeItemsArray[i], 1);
+      }
+    }
+    renderTheUI(toDos);
+  }
+
   function renderTheUI(toDos) {
 
     const todoList = document.getElementById('toDoList');
@@ -27,12 +45,48 @@ function onReady() {
 
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
+      const button = document.createElement('button');
+
 
       checkbox.type = "checkbox";
 
-      newLi.textContent = toDo.title;
+      checkbox.onchange = function(){
+        var pNode = button.parentNode;
+        var todoText = pNode.textContent.replace("Remove ToDo", "");
+        console.log("toDos count: " + toDos.length);
+        for (var i = 0; i < toDos.length; i++){
+          var title = toDos[i].title;
+          console.log(title);
+          if (toDos[i].title === todoText) {
+            toDos[i].complete = checkbox.checked;
+            console.log(toDos[i].title + " is completed: " + toDos[i].complete);
+          }
+        }
+      }
 
+      button.onclick = function(){
+        var pNode = button.parentNode;
+        var todoText = pNode.textContent.replace("Remove ToDo", "");
+        var removeIndex = 0;
+        console.log("toDos count: " + toDos.length);
+        for (var i = 0; i < toDos.length; i++){
+          var title = toDos[i].title;
+          console.log(title);
+          if (toDos[i].title === todoText) {
+            removeIndex = i;
+          }
+        }
+        toDos.splice(removeIndex, 1);
+        console.log(removeIndex);
+        console.log("toDos count: " + toDos.length);
+        renderTheUI(toDos);
+      }
+
+      var newLiTitle = document.createTextNode(toDo.title);
       newLi.appendChild(checkbox);
+      newLi.appendChild(newLiTitle);
+      button.textContent = "Remove ToDo";
+      newLi.appendChild(button);
       todoList.appendChild(newLi);
     });
   }
